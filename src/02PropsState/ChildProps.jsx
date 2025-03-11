50dc0ea53abd5a3e84bf3d72e7f443d88a06d36c
mikki3232go <66340888+mikki3232go@users.noreply.github.com>
2025-03-11 15:41:48 +0900

    Create ChildProps.jsx
    2025 Add Children Props

---
+++ src/02PropsState/ChildProps.jsx
@@ -0,0 +1,65 @@
+﻿function Card({ title, children }) {
+
+  return (
+    <div>
+      <div className="card">
+        <div className="card-content">
+          <h1>{title}</h1>
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+function Card1() {
+  return (
+    <div>
+      <div className="card">
+        <div className="card-content">
+          <h1>Photo</h1>
+          <img
+            className="avatar"
+            src="https://i.imgur.com/OKS67lhm.jpg"
+            alt="Aklilu Lemma"
+            width={70}
+            height={70}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+function Card2() {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <h1>About</h1>
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
+          natural treatment to schistosomiasis.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default function Profile() {
+  return (
+    <div>
+      <Card title ="사진">
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={70}
+          height={70}
+        />
+      </Card>
+      <Card title = "내맘대로">
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian scientist who discovered a
+          natural treatment to schistosomiasis.
+        </p>
+      </Card>
+    </div>
+  );
+}
