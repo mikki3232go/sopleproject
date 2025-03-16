c42bc045f6ff8731fb790b09334c4f29d129e4ee
smG <mikki3232go@gmail.com>
2025-03-16 17:13:04 +0900

    App.jsx변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,38 @@
-import ReactDOM from "react-dom";
+import ReactDOM from 'react-dom';
 
+import UserAdd from './06/UserAdd';
+import UserDelete from './06/UserDelete';
+import UserModify from './06/UserModify';
+import UseRefAdd from './06/UseRefAdd';
+import MsgListLast from './07/MsgListLast';
+import UseEffect2 from './07Hooks/UseEffect2';
+import MsgListLast2 from './07/MsgListLast2';
+import CommentList from './01comment/CommentList.jsx';
+import UseEffect1 from './07Hooks/useEffect1';
+import ReactFacts from './ReactFacts/ReactFacts.jsx';
 
-import UserAdd from "./06/UserAdd";
-import UserDelete from "./06/UserDelete";
-import UserModify from "./06/UserModify"
-import UseRefAdd from "./06/UseRefAdd";
-import MsgListLast from "./07/MsgListLast";
-import UseEffect2 from "./07Hooks/UseEffect2"
-import MsgListLast2 from "./07/MsgListLast2"
-
-import Accomodate from "./07Hooks/Accomodate"
-import UseEffect1 from "./07Hooks/useEffect1"
-
+import PostList31 from './03MapTest/PostList3-1.jsx';
+import PostList3 from './03MapTest/PostList3.jsx';
+import PostList2 from './03MapTest/PostList2.jsx';
+import PostList from './03MapTest/postList.jsx';
 export default function App() {
   return (
-   // <UseRefAdd />
-   // <UserAdd />
- //<UserDelete />
- //  <UserModify />
- //
-// <MsgListLast />
- //<MsgListLast2 />
- <UseEffect1 />
- //<MsgListLast />
-  )
-   
+    // <UseRefAdd />
+    // <UserAdd />
+    //<UserDelete />
+    //  <UserModify />
+    //
+    // <MsgListLast />
+    //<MsgListLast2 />
+    // <ReactFacts />
+    //<MsgListLast />
+    // <CommentList />
+    <div>
+      1<PostList />
+      2<PostList2 />
+      3<PostList31 />
+    </div>
+  );
 }
 /* git add .
 git commit -m "메시지"
