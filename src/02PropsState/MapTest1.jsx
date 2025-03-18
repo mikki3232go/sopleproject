364435c69653b51a054f18604ae3723df1ecba0b
mikki3232go <66340888+mikki3232go@users.noreply.github.com>
2025-03-18 23:20:53 +0900

    Update MapTest1.jsx

--- src/02PropsState/MapTest1.jsx
+++ src/02PropsState/MapTest1.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+mport React from "react";
+
+let user = { name: "soma", age: 20 };
+let { name, age } = user;
+const cars = ["Ford", "BMW", "Audi"];
+let foo = ["one", "two", "three"];
 
 function Car(props) {
-  return <li>I am a { props.brand }</li>;
+  return <li>I am a {props.brand}</li>;
 }
-
 function MapTest1() {
-  const cars = ['Ford', 'BMW', 'Audi'];
   return (
     <>
-	    <h1>차고에 있는 차 목록</h1>
-	    <ul>
-        {cars.map((car) => <Car brand={car} />)}
+      <h1>foo 배열 출력하기</h1>
+      <ul>
+        {foo.map((item) => (
+          // <Foo data={item} />
+          <li>{item}</li>
+        ))}
+        {/*foo 배열에 있는 아이템들을 맵으로 요소 생성*/}
+      </ul>
+      <h1>차고에 있는 차 목록</h1>
+      <ul>
+        {/*cars 배열 요소를 Car컴포넌트의 brand로 전송하여 컴포넌트 리턴 */}
+        {cars.map((car) => (
+          <Car brand={car} />
+        ))}
       </ul>
     </>
   );
