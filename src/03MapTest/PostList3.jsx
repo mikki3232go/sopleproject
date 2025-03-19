d84a1fc83f4e305d55bc30e945eac677bfab8138
mikki3232go <66340888+mikki3232go@users.noreply.github.com>
2025-03-19 19:24:31 +0900

    Update PostList3.jsx

--- src/03MapTest/PostList3.jsx
+++ src/03MapTest/PostList3.jsx
@@ -1,99 +1,81 @@
-import React, { useState } from 'react';
-import './postList.css';
-
-const initialComments = [
-  {
-    name: '오주현',
-    comment: '안녕하세요. 오주현입니다.',
-    time: '2시간 전',
-    avatar: 'https://www.w3schools.com/howto/img_avatar.png', // 기본 아바타 이미지 URL
-    likes: 0,
-  },
-  {
-    name: '',
-    comment: '리액트로 프로젝트 만들고 있습니다.',
-    time: '3시간 전',
-    avatar: 'https://www.w3schools.com/howto/img_avatar2.png', // 기본 아바타 이미지 URL
-    likes: 0,
-  },
-  {
-    name: '백도준',
-    comment: '리액트 재미있어요.',
-    time: '5시간 전',
-    avatar: ' https://www.w3schools.com/w3images/avatar6.png', // 기본 아바타 이미지 URL
-    likes: 0,
-  },
-  {
-    name: '공덕현',
-    comment: '리액트 벌써 3주차라니........',
-    time: '5시간 전',
-    avatar: ' https://www.w3schools.com/w3images/avatar5.png', // 기본 아바타 이미지 URL
-    likes: 0,
-  },
-];
-
-const Comment = ({ name, comment, time, avatar, likes, onDelete, onLike }) => (
-  <div className="comment">
-    <img className="avatar" src={avatar} alt="user-avatar" />
-    <div className="comment-details">
-      <div className="user-name">{name}</div>
-      <div className="comment-text">{comment}</div>
-      <div className="comment-time">
-        {time}
-        {'   '}
-        <span className="like-button" onClick={onLike}>
-          ❤️
-        </span>
-        <span className="like-count">{likes}</span>
-      </div>
+import "./postList.css";
+import { useState } from "react";
+export const posts = [
+    {
+      name: '오주현',
+      comment: '안녕하세요. 오주현입니다.',
+      time: '2시간 전',
+      avatar: 'https://www.w3schools.com/howto/img_avatar.png', // 기본 아바타 이미지 URL
+      like : 0,
+    },
+    {
+      name: '문소정',
+      comment: '리액트로 프로젝트 만들고 있습니다.',
+      time: '3시간 전',
+      avatar: 'https://www.w3schools.com/howto/img_avatar2.png', // 기본 아바타 이미지 URL
+      like : 0,
+    },
+    {
+      name: '황지영',
+      comment: '리액트 재미있어요.',
+      time: '5시간 전',
+      avatar: ' https://www.w3schools.com/w3images/avatar6.png', // 기본 아바타 이미지 URL
+      like : 0,
+    },
+    {
+      name: '공덕현',
+      comment: '리액트 벌써 3주차라니........',
+      time: '5시간 전',
+      avatar: ' https://www.w3schools.com/w3images/avatar5.png', // 기본 아바타 이미지 URL
+      like : 0,
+    },
+  ];
+ function Post(props){
+    const {name, comment, time, avatar,like, onLike,onDelete} = props;
+    const [count, setCount] = useState(0);
+    return(
+        <div className="comment">
+        <img className="avatar" src = {avatar}   />
+        <div className="comment-details"> 
+            <div className="user-name">{name}</div>  
+            <div className="comment-text">{comment}</div>
+            <span onClick = {onLike}>{' 💕💕  '}{like}</span> 
+            <div className="comment-time">{time}</div>  
+            <div><button onClick ={onDelete}>삭제</button></div>                  
+        </div>
     </div>
-    <div>
-      <button className="delete-button" onClick={onDelete}>
-        삭제
-      </button>
-    </div>
-  </div>
-);
+    );
+ }
+  export default function PostList2(){
+    const [lists, setLists] = useState(posts);
+    function onLikehandle(index){
+      
+        const newComments =[...lists];
+        newComments[index].like +=1;
+        setLists(newComments);
+        console.log(newComments[index]);
 
-const PostList3 = () => {
-  const [comments, setComments] = useState(initialComments);
-
-  // 댓글 삭제 함수
-  const handleDelete = (index) => {
-    setComments((prevComments) => prevComments.filter((_, i) => i !== index));
-  };
-
-  // 좋아요 증가 함수
-  const handleLike = (index) => {
-    setComments((prevComments) => {
-      const newComments = [...prevComments];
-      newComments[index].likes += 1;
-      return newComments;
-    });
-  };
-
-  return (
-    <div className="comment-list">
-      {comments.map((comment, index) => (
-        <Comment
-          key={index}
-          name={comment.name}
-          comment={comment.comment}
-          time={comment.time}
-          avatar={comment.avatar}
-          likes={comment.likes}
-          onDelete={() => handleDelete(index)} // 삭제 함수 전달
-          onLike ={()=> {
-                const newComments =[...comments];
+      
+    }
+    return(
+        <div className = "comment-list">
+            { lists.map((comment,index) =>(
+               <Post key ={index} name = {comment.name} 
+               comment = {comment.comment} time = {comment.time} 
+               avatar ={comment.avatar} like ={comment.like} 
+               onLike ={()=> {
+                const newComments =[...lists];
                 newComments[index].like +=1;
                 setLists(newComments);
                 console.log(newComments[index]);
-                
-            }} // 좋아요 함수 전달
-        />
-      ))}
-    </div>
-  );
-};
+              }}
+              onDelete ={()=> {
+                const result = lists.filter((comment,i) =>index !== i);
+                setLists(result);
+              }}
+               />
 
-export default PostList3;
+            ))}
+        </div>
+    )
+  }
