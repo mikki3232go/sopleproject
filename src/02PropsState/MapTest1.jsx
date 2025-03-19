8a2fd67076a247587dd09f523aefecacc15b7d0f
mikki3232go <66340888+mikki3232go@users.noreply.github.com>
2025-03-19 11:40:34 +0900

    Update MapTest1.jsx

--- src/02PropsState/MapTest1.jsx
+++ src/02PropsState/MapTest1.jsx
@@ -1,5 +1,4 @@
-mport React from "react";
-
+import React from "react";
 let user = { name: "soma", age: 20 };
 let { name, age } = user;
 const cars = ["Ford", "BMW", "Audi"];
@@ -8,27 +7,31 @@ let foo = ["one", "two", "three"];
 function Car(props) {
   return <li>I am a {props.brand}</li>;
 }
-function MapTest1() {
+
+export default function MapTest1() {
   return (
     <>
       <h1>foo 배열 출력하기</h1>
       <ul>
-        {foo.map((item) => (
-          // <Foo data={item} />
-          <li>{item}</li>
+        <li>{foo[0]}</li>
+        <li>{foo[1]}</li>
+        <li>{foo[2]}</li>
+        {foo.map((item)=>(
+            <li>{item}</li>
         ))}
-        {/*foo 배열에 있는 아이템들을 맵으로 요소 생성*/}
+        
       </ul>
       <h1>차고에 있는 차 목록</h1>
       <ul>
+        <Car brand ={cars[0]} />
+        <Car brand ={cars[1]} />
+        <Car brand ={cars[2]} />
         {/*cars 배열 요소를 Car컴포넌트의 brand로 전송하여 컴포넌트 리턴 */}
-        {cars.map((car) => (
-          <Car brand={car} />
+        {cars.map((car) =>(
+          <Car brand = {car} />
         ))}
+        
       </ul>
     </>
   );
 }
-export default MapTest1;
-
- 
