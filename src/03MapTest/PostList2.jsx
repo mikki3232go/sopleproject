50460c6a2c6e12cd4667abf7fcf8938e8c5a9b1d
mikki3232go <66340888+mikki3232go@users.noreply.github.com>
2025-03-25 18:53:36 +0900

    Update PostList2.jsx

--- src/03MapTest/PostList2.jsx
+++ src/03MapTest/PostList2.jsx
@@ -3,19 +3,19 @@ import './postList.css';
 import { posts } from './post';
 
 function Post({ name, comment, time, avatar, onDelete }) {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0); //1. 좋아요 useState
   return (
     <div className="comment">
       <img className="avatar" src={avatar} alt="user-avatar" />
       <div className="comment-details">
         <div className="user-name">{name}</div>
         <div className="comment-text">{comment}</div>
-        <span onClick={() => setCount(count + 1)}>{' ♥️ '}</span>
-        <span>{count}</span>
+        <span onClick={() => setCount(count + 1)}>{' ♥️ '}</span> {/*2. <span>태그로 좋아요 추가 */}
+        <span>{count}</span> {/*3. 좋아요 숫자 나타내기 */}
         <div className="comment-time">{time}</div>
       </div>
       <div>
-        <button onClick={onDelete}>삭제</button>
+        <button onClick={onDelete}>삭제</button> {/* 4. 삭제 버튼 */}
       </div>
     </div>
   );
