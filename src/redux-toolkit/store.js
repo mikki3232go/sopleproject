3ed81f123ee570682a8d9fc723e4ef81fc65a4ca
mikki3232go <66340888+mikki3232go@users.noreply.github.com>
2025-05-20 22:12:54 +0900

    Update store.js

--- src/redux-toolkit/store.js
+++ src/redux-toolkit/store.js
@@ -1,5 +1,16 @@
-//store.js
+1.프로젝트 생성 및 필수 패키지 설치
 
+  /*npm create vite@latest redux-toolkit-counter
+  cd redux-toolkit-counter
+  npm install @reduxjs/toolkit react-redux */
+
+
+2. Slice생성
+
+
+//step1 : src/app/store.js 파일에 counterSlice를 설정
+//step 2. configureStore( ) 안에 등록
+//src/app/store.js */
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 export const counterSlice = createSlice({
   name: 'counter',
@@ -21,15 +32,17 @@ export const counterSlice = createSlice({
 //counter의 action을 export
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
-3. Redux 설정 파일 생성
-src/app/store.js 파일에, Redux 스토어를 설정합니다.(state들을 보관하는 파일)
-............
+//3. Redux 설정 파일 생성
+//src/app/store.js 파일에, Redux 스토어를 설정합니다.(state들을 보관하는 파일)
+//............
 export const store = configureStore({ //3.Redux 스토어를 설정
    reducer: {
      counter: counter.reducer,
    },
  });
-4 .Redux Provider 설정(index.jsx):App와 자식 컴포넌트들은 store state 마음대로 사용 가능
+
+
+//4 .Redux Provider 설정(index.jsx):App와 자식 컴포넌트들은 store state 마음대로 사용 가능
 import React from 'react';
  import ReactDOM from 'react-dom';
  import './index.css';
@@ -46,7 +59,7 @@ import React from 'react';
  );
 
 
-5. App.jsx에서 state가져와서 사용하기
+//5. App.jsx에서 state가져와서 사용하기
  import { useSelector, useDispatch } from "react-redux";
  import { increment, decrement, incrementByAmount } from "./store.js";
  export default function App(
